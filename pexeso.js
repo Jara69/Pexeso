93bb10ebef2609f0dc8e43ad70963a7ab6088d1b
Jara69 <jaroslav.havrda@educanet.cz>
2019-12-04 21:31:53 +0100

    pexeso

--- pexeso.js
+++ pexeso.js
@@ -1,8 +1,1 @@
-const karty = document.querySelectorAll('.pexeso-card');
-
-function flip() {
-    console.log('click');
-    console.log(this);
-}
-
-karty.forEach(karta => karta.addEventListener('click', flip));
+IDK
